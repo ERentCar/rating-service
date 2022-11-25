18c3f8e3fa6030cbf6f1dbae1673ebf1bb5905ee
Sergio Guanilo <u20181h016@upc.edu.pe>
2022-11-24 22:21:26 -0500

    for deploy

--- src/main/java/com/rentCar/ratingService/Services/impls/ApiCall.java
+++ src/main/java/com/rentCar/ratingService/Services/impls/ApiCall.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApiCall {
     @Autowired
     RestTemplate restTemplate;
-    public void setRatingCar(Long carId,Double rating)throws RestClientException {
+    /*public void setRatingCar(Long carId,Double rating)throws RestClientException {
         restTemplate.exchange("http://localhost:8115/api/v1/cars/"+carId+"/rating/"+rating,
                 HttpMethod.PUT,null,void.class);
     }
@@ -20,6 +20,18 @@ public class ApiCall {
     }
     public void setRatingClient(Long clientId,Double rating)throws RestClientException {
         restTemplate.exchange("http://localhost:8105/api/v1/clients/"+clientId+"/rating/"+rating,
+                HttpMethod.PUT,null,void.class);
+    }*/
+    public void setRatingCar(Long carId,Double rating)throws RestClientException {
+        restTemplate.exchange("https://cars-erentcar.azurewebsites.net/api/v1/cars/"+carId+"/rating/"+rating,
+                HttpMethod.PUT,null,void.class);
+    }
+    public void setRatingOwner(Long ownerId,Double rating)throws RestClientException {
+        restTemplate.exchange("https://users-erentcar.azurewebsites.net/api/v1/owners/"+ownerId+"/rating/"+rating,
+                HttpMethod.PUT,null,void.class);
+    }
+    public void setRatingClient(Long clientId,Double rating)throws RestClientException {
+        restTemplate.exchange("https://users-erentcar.azurewebsites.net/api/v1/clients/"+clientId+"/rating/"+rating,
                 HttpMethod.PUT,null,void.class);
     }
 }
